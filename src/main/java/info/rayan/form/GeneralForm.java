aa5b7579b0efba599b58a77d5220fa51a8272b0a
Hamid Samani <hamid1129@gmail.com>
2014-10-18 14:26:24 +0330

    Add related methods in abstract GeneralForm backing bean.

--- src/main/java/info/rayan/form/GeneralForm.java
+++ src/main/java/info/rayan/form/GeneralForm.java
@@ -2,6 +2,7 @@ package info.rayan.form;
 
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.context.Flash;
 
@@ -20,8 +21,16 @@ public abstract class GeneralForm implements Serializable {
 		return getFlash().get(key);
 	}
 
-	protected String redirect(String view) {
+	protected String navigateTo(String view) {
 		return String.format("%s?faces-redirect=true", view);
+	}
+
+	protected boolean isPostback() {
+		return getFacesContext().isPostback();
+	}
+
+	protected void addMessage(String summary) {
+		getFacesContext().addMessage(null, new FacesMessage(summary));
 	}
 
 	private Flash getFlash() {
